8792b2202be87444072fbfdb989c42bbca0edf6f
Moontasir Abtahee <mamoontasir.abtahee@gmail.com>
2025-08-20 14:45:36 +0600

    258. Add Digits

--- 258. Add Digits.py
+++ 258. Add Digits.py
@@ -9,7 +9,7 @@ class Solution(object):
 
             sum += num%10
             num = num//10
-            if num == 0 and sum > 10:
+            if num == 0 and sum >= 10:
                 num = sum
                 sum = 0
         return sum
